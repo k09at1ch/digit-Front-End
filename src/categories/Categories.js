a9bb283efcc1d3a1c7fd5cfb3ae6dabfba4cc5b7
k09at1ch <newwerasik228@gmail.com>
2025-02-24 19:35:53 +0200

    n

--- src/categories/Categories.js
+++ src/categories/Categories.js
@@ -1,37 +1,38 @@
 import React from "react";
 
 function Categories() {
-    
+  const aviableCategories = [
+    "Smartphones and Gadgets",
+    "Laptops and Computers",
+    "Televisions and Accessories",
+    "Home Appliances",
+    "Beauty and Health",
+    "Sports and Tourism",
+    "Home Products",
+    "Kids' Products",
+    "Toys and Games",
+    "Car Electronics",
+    "Stationery and Books",
+    "Food and Beverages",
+    "Tools and Repairs",
+    "Furniture",
+    "Fashion and Accessories",
+  ];
+
+
+  const eventListener = (category) => {
+    console.log(category);
+  };
     return (
       <div className="">
         <ul>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-          </li>
-          <li>
-            <button></button>
-        </li>
+          {aviableCategories.map((category, index) => (
+            <li key={index}>
+              <button onClick={() => eventListener(category)}>
+                {category}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     );
