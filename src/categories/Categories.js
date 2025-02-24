007fc6499295e2cc6f6ffcf96d8850613a498e4e
k09at1ch <newwerasik228@gmail.com>
2025-02-24 17:00:39 +0200

    add

---
+++ src/categories/Categories.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Categories() {
+    
+    return (
+      <div className="">
+        <ul>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+        </li>
+        </ul>
+      </div>
+    );
+}
+
+export default Categories
